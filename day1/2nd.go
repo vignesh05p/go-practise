83f218c31bbbfcf4acdad7b0026462ad5c813379
vignesh05p <vigneshprabhu050@gmail.com>
2025-08-11 20:55:10 +0530

    Initial commit: Add first.go

--- day1/2nd.go
+++ day1/2nd.go
@@ -1,10 +1,23 @@
-switch v := i.(type) {
-case string:
-	fmt.Println("It's a string:", v)
-case int:
-	fmt.Println("It's an int:", v)
-case bool:
-	fmt.Println("It's a bool:", v)
-default:
-	fmt.Println("Unknown type")
+package main
+
+import "fmt"
+
+func printType(i interface{}) {
+	switch v := i.(type) {
+	case string:
+		fmt.Println("String:", v)
+	case int:
+		fmt.Println("Integer:", v)
+	case float64:
+		fmt.Println("Float:", v)
+	default:
+		fmt.Println("Unknown type")
+	}
+}
+
+func main() {
+	printType("Hello")
+	printType(42)
+	printType(3.14)
+	printType(true)
 }
