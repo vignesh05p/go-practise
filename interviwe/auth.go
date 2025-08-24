5559a12de6de0123f7242114c8f42500438eab65
Vignesh prabhu <138098975+vignesh05p@users.noreply.github.com>
2025-08-24 20:13:56 +0530

    heh

--- interviwe/auth.go
+++ interviwe/auth.go
@@ -1,21 +1,36 @@
-//authentication middleware
-
+//write s simple logging middleware that prints the requests method and URL before passing the rquest to the next handler
 
 package main
-
 
 import (
 	"fmt"
 	"net/http"
+	"log"
+	"time"
+	
 )
 
-func  AuthMid(next http.Handler) http.Handler{
+func loggingMiddelware(next http.Handler) http.Handler{
+    
+    return http.HandlerFunc(func(w  http.ResponseWriter, r *http.Request){
+         log.Printf("Request received : %s %s", r.Method, r.URL.PATH)
+         next .serverHTTP(w,r)
+    
+    })
+}
 
-	return  http.HandlerFunc( func( w http.RsponseWriter, r *http.Request)){
 
-		apiKey := r.Header.Get("X-API-Key")
+func homeHandler(w http.ResponseWriter, r *http.Rquest){
+    fmt.Fprintln(w. "hello, world! THis is th main handler,")
+}
 
-		if
-	}
+
+func main(){
+    finalHanler :=loggingMiddleware(http.HandlerFunc(homeHandler))
+    http.Handle("/", finalHandler)
+    
+    
+    fmt.Println("server is running on ssserver")
+    log.Fatal(http.ListenAndServer("8080",nil))
 }
 
